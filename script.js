ff539f5a609e22c3d1ab34e34560550c38f54814
Gabriell0909 <gabrielalvesg2002@gmail.com>
2026-06-30 17:13:56 -0300

    Mudança para o padrão de projeto pedido

---
+++ script.js
@@ -0,0 +1,20 @@
+//CAPTURA O NOME DE JOGADOR
+let jogador1 = document.querySelector("#p1name");
+let jogador2 = document.querySelector("#p2name");
+
+//BUSCA O CAMPO DO NUMERO DE VITORIAS
+let pontuacaoP1 = document.querySelector("#nVitoriaP1")
+let pontuacaoP2 = document.querySelector("#nVitoriaP2")
+
+//BUSCA O CAMPO QUE REPRESENTA O NUMERO DE ROUNDS
+let rodadas = document.querySelector("#nRounds")
+
+//COLETA TODAS AS CELULAS
+const cells = document.querySelectorAll("celula")
+
+
+
+
+function jogadorAtual(){
+    
+}
